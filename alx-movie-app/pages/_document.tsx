be20880c4fa43194b451b3a49a096e1e52cada20
baby6969 <yafetzekarias23@fmail.com>
2025-10-26 16:56:19 -0400

    core updates

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -3,8 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="antialiased">
+      <Head>
+        {/* Web App Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Icons for different devices */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" sizes="192x192" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+
+        {/* Theme color */}
+        <meta name="theme-color" content="#FFFFFF" />
+      </Head>
+      <body>
         <Main />
         <NextScript />
       </body>
